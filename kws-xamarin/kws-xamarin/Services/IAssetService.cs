3b96343c82a9a3602d7e17781f4e8b9b66317d6e
Mahima Bhat <bhatmahima01@gmail.com>
2020-05-26 14:44:07 +0530

    Add project files.

---
+++ kws-xamarin/kws-xamarin/Services/IAssetService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace kws_xamarin
+{
+    public interface IAssetService
+    {
+        string GetAssetPath(string filename);
+    }
+}
---
+++ kws-xamarin/kws-xamarin/Services/IMicrophoneService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace kws_xamarin
+{
+    public interface IMicrophoneService
+    {
+        Task<bool> GetPermissionsAsync();
+        void OnRequestPermissionsResult(bool isGranted);
+    }
+}
